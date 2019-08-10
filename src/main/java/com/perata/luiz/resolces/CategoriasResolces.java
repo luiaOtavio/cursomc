85640900332270715f40ab99432089c8b08a2fa3
Luis Otavio <luizcanalgame@gmai.com>
2019-08-09 22:15:38 -0300

    Testando a primeira classe  de dominio

--- src/main/java/com/perata/luiz/resolces/CategoriasResolces.java
+++ src/main/java/com/perata/luiz/resolces/CategoriasResolces.java
@@ -1,14 +1,28 @@
 package com.perata.luiz.resolces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.perata.luiz.Domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriasResolces {
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {return "Rest esta funcionado";}
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
 	
 	
 }
